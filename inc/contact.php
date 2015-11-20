67b6ad40d00b24a098b62bfdc6d67d45835cd42b
Brian Jenney <brian_jenney@yahoo.com>
2015-11-19 21:34:55 -0800

    some media queries added to projects and main section

--- inc/contact.php
+++ inc/contact.php
@@ -1,111 +1,23 @@
+
 <?php
-/*
-* Contact Form Class
-*/
+// variables containing form input data
+$name = $_POST['contactname'];
+$email = $_POST['contactemail'];
+$message = $_POST['contactmessage'];
 
+//email display when opened
+$formcontent="From: $name \n Email: $email \n Message: $message";
 
-header('Cache-Control: no-cache, must-revalidate');
-header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-header('Content-type: application/json');
+//email that receives form info
+$recipient = 'brianjenney83@gmail.com'; //my email
 
-$admin_email = 'brianjenney83@gmail.com'; 
-$message_min_length = 5; 
+//subject line of email
+$subject = "Message from portfolio site";
 
+//tells you who it is from
+$mailheader = "From: $email \r\n";
 
-class Contact_Form{
-	function __construct($details, $email_admin, $message_min_length){
-		
-		$this->name = stripslashes($details['inputName']);
-		
-		
-		$this->email = trim($details['inputEmail']);
-		$this->subject = 'Email from Portfolio Site'; // Subject 
-		$this->message = stripslashes($details['inputMessage']);
-	
-		$this->email_admin = $email_admin;
-		$this->message_min_length = $message_min_length;
-		
-		$this->response_status = 1;
-		$this->response_html = '';
-	}
-
-
-	private function validateEmail(){
-		$regex = '/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i';
-	
-		if($this->email == '') { 
-			return false;
-		} else {
-			$string = preg_replace($regex, '', $this->email);
-		}
-	
-		return empty($string) ? true : false;
-	}
-
-
-	private function validateFields(){
-		// Check name
-		if(!$this->name)
-		{
-			$this->response_html .= '<div class="alert alert-warning alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Warning!</strong> Please enter your name</div>';
-			$this->response_status = 0;
-		}
-
-		// Check email
-		if(!$this->email)
-		{
-			$this->response_html .= '<div class="alert alert-warning alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Warning!</strong> Please enter your email address</div>';
-			$this->response_status = 0;
-		}
-		
-		// Check valid email
-		if($this->email && !$this->validateEmail())
-		{
-			$this->response_html .= '<div class="alert alert-warning alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Warning!</strong> Please enter a valid email address</div>';
-			$this->response_status = 0;
-		}
-		
-		// Check message length
-		if(!$this->message || strlen($this->message) < $this->message_min_length)
-		{
-		
-			$this->response_html .= '<div class="alert alert-warning alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Warning!</strong> Your messages needs to have at least '.$this->message_min_length.' characters</div>';
-			$this->response_status = 0;
-		}
-	}
-
-
-	private function sendEmail(){
-		$mail = mail($this->email_admin, $this->subject, $this->message,
-			 "From: ".$this->name." <".$this->email.">\r\n"
-			."Reply-To: ".$this->email."\r\n"
-		."X-Mailer: PHP/" . phpversion());
-	
-		if($mail)
-		{
-			$this->response_status = 1;
-			$this->response_html = '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Thank you</strong> Your message has been delivered</div>';
-		}
-	}
-
-
-	function sendRequest(){
-		$this->validateFields();
-		if($this->response_status)
-		{
-			$this->sendEmail();
-		}
-
-		$response = array();
-		$response['status'] = $this->response_status;	
-		$response['html'] = $this->response_html;
-		
-		echo json_encode($response);
-	}
-}
-
-
-$contact_form = new Contact_Form($_POST, $admin_email, $message_min_length);
-$contact_form->sendRequest();
-
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+$home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index.html';
+  header('Location: ' . $home_url);
 ?>
